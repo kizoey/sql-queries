58dcb43b733b6c240ca9e74f6de85bbd65015cae
DaYeon Ki <81248507+kizoey@users.noreply.github.com>
2021-03-27 23:50:39 +0900

    Create 특정 키워드, 카테고리의 상품등록수.sql

---
+++ Monthly/특정 키워드, 카테고리의 상품등록수.sql
@@ -0,0 +1,26 @@
+select count(distinct id) as "등록수"
+from product_info_TB
+where name like '%{{검색어}}%'
+    and create_date >= '{{From when}}' 
+    and create_date < '{{To when}}'
+    and (case
+        when '%{{카테고리}}%' = '%뷰티/미용%' then left(category_id, 3) = '410'
+        when '%{{카테고리}}%' = '%패션잡화%' then left(category_id, 3) = '400'
+        when '%{{카테고리}}%' = '%디지털/가전%' then left(category_id, 3) = '600'
+        when '%{{카테고리}}%' = '%여성의류%' then left(category_id, 3) = '310'
+        when '%{{카테고리}}%' = '%남성의류%' then left(category_id, 3) = '320'
+        when '%{{카테고리}}%' = '%구인구직%' then left(category_id, 3) = '240'
+        when '%{{카테고리}}%' = '%차량/오토바이%' then left(category_id, 3) = '750'
+        when '%{{카테고리}}%' = '%스포츠/레저%' then left(category_id, 3) = '700'
+        when '%{{카테고리}}%' = '%유아동/출산%' then left(category_id, 3) = '500'
+        when '%{{카테고리}}%' = '%지역서비스%' then left(category_id, 3) = '220'
+        when '%{{카테고리}}%' = '%스타굿즈%' then left(category_id, 3) = '910'
+        when '%{{카테고리}}%' = '%도서/티켓/취미/애완%' then left(category_id, 3) = '900'
+        when '%{{카테고리}}%' = '%생활/문구/가구/식품%' then left(category_id, 3) = '800'
+        when '%{{카테고리}}%' = '%구패션의류%' then left(category_id, 3) = '300'
+        when '%{{카테고리}}%' = '%원룸/함께살아요%' then left(category_id, 3) = '230'
+        when '%{{카테고리}}%' = '%재능%' then left(category_id, 3) = '210'
+        when '%{{카테고리}}%' = '%기타%' then left(category_id, 3) = '999'
+        when '%{{카테고리}}%' = '%커뮤니티%' then left(category_id, 3) = '100'
+        when '%{{카테고리}}%' = '%번개나눔%' then left(category_id, 3) = '200'
+        end)
